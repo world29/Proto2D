f4a018bab07e281fe80d976050acc930ef970811
world29 <ryo-29.2929@hotmail.co.jp>
2019-07-31 00:16:55 +0900

    アセットを追加

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text replayText;
+
+    private bool isGameOver;
+    private bool isGameClear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+        isGameClear = false;
+        replayText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameOver && !isGameClear)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    public bool IsGameOver()
+    {
+        return isGameOver;
+    }
+
+    public bool IsGameClear()
+    {
+        return isGameClear;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        replayText.text = "Hit Enter to replay!";
+    }
+
+    public void GameClear()
+    {
+        isGameClear = true;
+        replayText.text = "Congratulations!\nHit Enter to replay!";
+    }
+}
---
+++ Assets/Scripts/GoalArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalArea : MonoBehaviour
+{
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameController.GameClear();
+            //Instantiate(explosion, transform.position, transform.rotation);
+            //Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+            //Destroy(collision.gameObject);
+        }
+    }
+}
