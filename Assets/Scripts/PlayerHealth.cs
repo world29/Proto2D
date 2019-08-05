47242b40f57da8940c4f1611a25867e2fc8c58f8
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-06 00:00:58 +0900

    プレイヤーに体力コンポーネントと体力バーの UI を追加

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : Damageable
+{
+    public Slider slider;
+    public Image fillImage;
+    public Color fullHealthColor = Color.green;
+    public Color zeroHealthColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+
+        SetHealthUI();
+    }
+
+    protected override void OnTakeDamage()
+    {
+        SetHealthUI();
+    }
+
+    protected override void OnDeath()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void SetHealthUI()
+    {
+        slider.value = currentHealth;
+
+        fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, currentHealth / startingHealth);
+    }
+}
