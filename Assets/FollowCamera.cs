32f8daeff85fe097ce6d488527f5b78f724c72ec
takataka1116 <53572667+takataka1116@users.noreply.github.com>
2019-08-10 18:26:49 +0900

    ステージやキャラなどのスケールをデフォルトの1倍になるように拡大　背景画像仮置き

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 【背景のコントロール用クラス】
+///     背景は3枚、カメラから見切れたら回り込む
+/// </summary>
+public class BackGroundController : MonoBehaviour
+{
+
+    // 背景の枚数
+    int spriteCount = 3;
+    // 背景が回り込み
+    float rightOffset = 1.45f;
+    float leftOffset = -0.4f;
+
+    Transform bgTfm;
+    SpriteRenderer mySpriteRndr;
+    float width;
+
+    void Start()
+    {
+        bgTfm = transform;
+        mySpriteRndr = GetComponent<SpriteRenderer>();
+        width = mySpriteRndr.bounds.size.x;
+    }
+
+
+    void Update()
+    {
+        // 座標変換
+        Vector3 myViewport = Camera.main.WorldToViewportPoint(bgTfm.position);
+
+        // 背景の回り込み(カメラがX軸プラス方向に移動時)
+        if (myViewport.x < leftOffset)
+        {
+            bgTfm.position += Vector3.right * (width * spriteCount);
+        }
+        // 背景の回り込み(カメラがX軸マイナス方向に移動時)
+        else if (myViewport.x > rightOffset)
+        {
+            bgTfm.position -= Vector3.right * (width * spriteCount);
+        }
+    }
+}
