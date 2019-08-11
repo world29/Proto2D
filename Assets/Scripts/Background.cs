251575402ac0e6a24265ffa80ffb279f2e3c8be9
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-12 04:01:27 +0900

    背景オブジェクト用のスクリプトを追加
    カメラに追従する動きを実装

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [Header("カメラ追従モード")]
+    /*public*/ bool followCamera = true;
+
+    [Range(1, 20), Header("視差(カメラ追従モードが無効の場合のみ)")]
+    /*public*/ float parallax = 1;
+
+    private void Start()
+    {
+        Vector3 camPos = Camera.main.transform.position;
+        transform.position = new Vector3(transform.position.x, camPos.y, transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        if (followCamera)
+        {
+            float shiftY = Camera.main.transform.position.y - transform.position.y;
+            transform.Translate(0, shiftY, 0);
+        }
+        else
+        {
+            float newY = (transform.position.y - Camera.main.transform.position.y) / parallax;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
+    }
+}
