5455d840973cdd966742ce7577518c951b2361f9
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-14 17:39:08 +0900

    プライヤーの踏みつけ攻撃判定 (Stomper object) と敵の踏みつけ受付判定 (Stompable prefab) を追加

---
+++ Assets/Scripts/Stompable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Stompable : MonoBehaviour
+{
+    private Enemy enemy;
+
+    private void Start()
+    {
+        // 親 gameObject が Enemy コンポーネントを持つ必要がある
+        enemy = transform.parent.gameObject.GetComponent<Enemy>();
+        Debug.Assert(enemy != null);
+    }
+
+    private void OnDrawGizmos()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Gizmos.color = new Color(0, 0, 1, .5f);
+        Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Stomper other = collision.gameObject.GetComponent<Stomper>();
+        if (other)
+        {
+            Debug.Log("OnTriggerEnter in Stompable");
+
+            // ダメージを受ける
+            enemy.TakeDamage(other.damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Stomper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Stomper : MonoBehaviour
+{
+    [Header("踏みつけによって与えるダメージ")]
+    public float damage = 1;
+
+    private Player player;
+
+    private void Start()
+    {
+        // 親 gameObject が Player コンポーネントを持つ必要がある
+        player = transform.parent.gameObject.GetComponent<Player>();
+        Debug.Assert(player != null);
+    }
+
+    private void OnDrawGizmos()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Gizmos.color = new Color(1, 0, 0, .5f);
+        Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Stompable receiver = collision.gameObject.GetComponent<Stompable>();
+        if (receiver)
+        {
+            Debug.Log("OnTriggerEnter in Stomper");
+
+            player.Hop();
+        }
+    }
+}
