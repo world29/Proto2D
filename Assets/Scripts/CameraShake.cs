f494b69a70a384fcdeea7467c14d89aca7025ad1
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-16 01:34:44 +0900

    カメラ揺れスクリプトを追加

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float shakeAmount;
+
+    private void Awake()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        // デバッグ用
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Shake(.1f, .2f);
+        }
+    }
+
+    public void Shake(float amount, float length)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("DoShake", 0, .01f);
+        Invoke("StopShake", length);
+    }
+
+    void DoShake()
+    {
+        if (shakeAmount > 0)
+        {
+            Vector3 camPos = mainCamera.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            mainCamera.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("DoShake");
+        mainCamera.transform.localPosition = Vector3.zero;
+    }
+}
