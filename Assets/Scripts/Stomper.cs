202edf5668a2b8c4f841cbf3fca3a932ee1522c3
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-16 01:35:29 +0900

    プレイヤーが敵を踏んだときにカメラ揺れを発生させるようにした

--- Assets/Scripts/Stomper.cs
+++ Assets/Scripts/Stomper.cs
@@ -11,13 +11,23 @@ public class Stomper : MonoBehaviour
     [Header("踏みつけ時のヒットストップ時間")]
     public float hitStopDuration = .1f;
 
+    [Header("攻撃がヒットしたときのカメラ揺れの大きさ")]
+    public float cameraShakeAmount = .1f;
+
+    [Header("攻撃がヒットしたときのカメラ揺れの持続時間")]
+    public float cameraShakeDuration = .2f;
+
     private Player player;
+    private CameraShake cameraShake;
 
     private void Start()
     {
         // 親 gameObject が Player コンポーネントを持つ必要がある
         player = transform.parent.gameObject.GetComponent<Player>();
         Debug.Assert(player != null);
+
+        cameraShake = GameObject.Find("Main Camera").GetComponent<CameraShake>();
+        Debug.Assert(cameraShake != null);
     }
 
     private void OnDrawGizmos()
@@ -34,6 +44,7 @@ public class Stomper : MonoBehaviour
         {
             Debug.Log("OnTriggerEnter in Stomper");
 
+            cameraShake.Shake(cameraShakeAmount, cameraShakeDuration);
             player.HitStop(hitStopDuration);
             player.Hop();
         }
