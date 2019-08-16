609506fa03ac5e982ec7db0fd83a135e86c90ac4
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-16 16:47:36 +0900

    デバッグログを画面にオーバーレイ表示する UI プレハブを追加

---
+++ Assets/Scripts/DebugLogScroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugLogScroll : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public Text contentText;
+
+    private void Awake()
+    {
+        Application.logMessageReceived += OnLogMessageReceived;
+    }
+
+    private void Start()
+    {
+        contentText.text = "";
+    }
+
+    private void OnDestroy()
+    {
+        Application.logMessageReceived -= OnLogMessageReceived;
+    }
+
+    private void OnLogMessageReceived(string logText, string stackTrace, LogType logType)
+    {
+        if (!string.IsNullOrEmpty(logText))
+        {
+            contentText.text += logText + System.Environment.NewLine;
+
+            scrollRect.verticalNormalizedPosition = 0;
+        }
+    }
+}
