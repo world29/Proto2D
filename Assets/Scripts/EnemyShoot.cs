7586f869a816825c5a1e8446b795548d5ee9a164
world29 <ryo-29.2929@hotmail.co.jp>
2019-08-25 02:32:10 +0900

    敵が一定間隔で弾を発射する仕組みを実装

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    [Header("発射するオブジェクト")]
+    public Projectile projectilePrefab;
+    [Header("発射の始点")]
+    public Transform shootTransform;
+    [Header("初速度")]
+    public Vector3 shootVelocity;
+    [Header("発射の時間間隔")]
+    public float shootInterval;
+
+    private float shootTimer;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (shootTimer >= shootInterval)
+        {
+            Shoot();
+
+            shootTimer = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        Projectile clone = Instantiate(projectilePrefab, shootTransform.position, shootTransform.rotation) as Projectile;
+
+        // 初速度の水平方向に対してオブジェクトの向きを反映する
+        Vector3 velocity = shootVelocity;
+        if (GetComponent<Controller2D>().collisions.faceDir < 0)
+        {
+            velocity.x *= -1;
+        }
+        clone.initialVelocity = velocity;
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float gravity = 20;
+    public Vector3 initialVelocity;
+
+    private Vector3 velocity;
+    
+    void Start()
+    {
+        velocity = initialVelocity;
+    }
+
+    void Update()
+    {
+        velocity.y -= gravity * Time.deltaTime;
+
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("GameArea"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
