accb4cf6a7f36ed7e4fb7f9450adedd2ce057052
takataka1116 <53572667+takataka1116@users.noreply.github.com>
2019-09-01 11:24:36 +0900

    敵　自走砲　追加　スプライトをオフセット出来るように敵プレハブの親子関係調整

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EnemyGround), typeof(Animator))]
+[RequireComponent(typeof(EnemyGround))]
 public class EnemyShoot : MonoBehaviour
 {
     [Header("発射するオブジェクト")]
