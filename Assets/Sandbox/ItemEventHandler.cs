42dd3583df2ce96052a71fa254fbbed498624d99
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-08 20:48:07 +0900

    アイテムイベントのスクリプトの追加漏れ

---
+++ Assets/Sandbox/ItemEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum ItemType
+{
+    Hopper, // ホッパー
+}
+
+public interface IItemReceiver : IEventSystemHandler
+{
+    void OnPickupItem(ItemType type, GameObject sender);
+}
