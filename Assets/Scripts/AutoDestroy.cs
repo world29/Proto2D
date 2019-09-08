6d99e8235ee4c9993b7f39ad18c5a3786ab0f78d
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-08 20:22:01 +0900

    リングエフェクトの再生終了時に自動で削除するようにした
    - すべての子 Animator のステートが End になったら、再生終了とみなす

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    Animator[] animators;
+
+    void Start()
+    {
+        animators = GetComponentsInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < animators.GetLength(0); i++)
+        {
+            if (!animators[i].GetCurrentAnimatorStateInfo(0).IsName("End"))
+            {
+                // 再生中のアニメーションが存在する
+                return;
+            }
+        }
+
+        // すべてのアニメーション再生が終了したら自身を削除する
+        Destroy(gameObject);
+    }
+}
