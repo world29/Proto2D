580934707cb11b97565c03b51633b9ae6a849cec
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-10 22:42:07 +0900

    値の変更時に登録したコールバックを呼び出すオブジェクトを追加
    プリミティブ型のみ対応

---
+++ Assets/Scripts/NotificationObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class NotificationObject<T>
+{
+    public delegate void NotificationAction(T t);
+
+    private T data;
+
+    public NotificationObject()
+    {
+        Value = default(T);
+    }
+
+    public NotificationObject(T t)
+    {
+        Value = t;
+    }
+
+    ~NotificationObject()
+    {
+        Dispose();
+    }
+
+    public UnityAction<T> OnChanged;
+
+    public T Value
+    {
+        get
+        {
+            return data;
+        }
+        set
+        {
+            data = value;
+            Notify();
+        }
+    }
+
+    private void Notify()
+    {
+        if (OnChanged != null)
+            OnChanged(data);
+    }
+
+    public void Dispose()
+    {
+        OnChanged = null;
+    }
+}
