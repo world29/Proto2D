ed5e4616c4ed0fe327b8c5f6dea973b90766dd84
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-10 23:16:28 +0900

    シーンに配置されたオブジェクトを指定したプレハブに一括で置換するエディタスクリプトを追加

---
+++ Assets/Scripts/Editor/PrefabReplacement.cs
@@ -0,0 +1,84 @@
+﻿// 参考 URL
+// http://carving.roguelive.chicappa.jp/?eid=44
+
+using UnityEngine;
+using UnityEditor;
+
+class PrefabReplacement : EditorWindow
+{
+    string[] prefabs;
+    int selected;
+    Vector2 scrollPosition;
+
+    // プレハブ差し替え
+    [MenuItem("Assets/PrefabReplacement %&h")]
+    static void ReplacementPrefabs()
+    {
+        GetWindow<PrefabReplacement>();
+    }
+
+    // 起動
+    void Awake()
+    {
+        // プレハブをリストアップ
+        this.prefabs = System.IO.Directory.GetFiles(
+    Application.dataPath, "*.prefab", System.IO.SearchOption.AllDirectories);
+
+        // ディスクのパスからアセットパスに変換
+        for (int i = 0, last = this.prefabs.Length; i < last; ++i)
+        {
+            this.prefabs[i] =
+            "Assets" + this.prefabs[i].Substring(Application.dataPath.Length);
+        }
+        this.selected = 0;
+        this.scrollPosition = Vector2.zero;
+    }
+
+    // ウィンドウの処理
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.BeginVertical();
+        this.scrollPosition = GUILayout.BeginScrollView(this.scrollPosition);
+        this.selected = GUILayout.SelectionGrid(this.selected, this.prefabs, 1);
+        GUILayout.EndScrollView();
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+        GUILayout.Space(20);
+        if (GUILayout.Button("OK"))
+        {
+            Replacement(this.prefabs[this.selected]);
+        }
+        GUILayout.Space(10);
+        if (GUILayout.Button("Close"))
+        {
+            this.Close();
+        }
+    }
+
+    // プレハブを置換
+    public static void Replacement(string path)
+    {
+        // 差し替えるプレハブを読み込み
+        GameObject newPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+        // ヒエラルキーウィンドウで選択されているGameObjectを順に処理
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            // 新しいプレハブをインスタンス化
+            GameObject newObj =
+      PrefabUtility.InstantiatePrefab(newPrefab) as GameObject;
+
+            // 差し替え前のGameObjectの情報をコピー
+            newObj.transform.position = obj.transform.position;
+            newObj.transform.eulerAngles = obj.transform.eulerAngles;
+            newObj.transform.SetParent(obj.transform.parent);
+
+            // 古いプレハブ削除
+            DestroyImmediate(obj);
+        }
+
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(
+        UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+    }
+}
