938a8c407bb76952c67dda8d6d10e4a2bca6b9e6
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-12 03:41:55 +0900

    ジョイスティックの更新 (設定した領域内でのみタッチを開始できるようにした)

--- Assets/Scripts/CustomFloatingJoystick.cs
+++ Assets/Scripts/CustomFloatingJoystick.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public struct FlickData
-{
-
-}
-
 //
 // if (joystick.Flicked) {
 //     joystick.Directional;
@@ -20,14 +15,16 @@ public struct FlickData
 
 public class CustomFloatingJoystick : FloatingJoystick
 {
+    public float flickThresholdTime = .2f;
+    public Rect touchableRect = new Rect(0, 0, 1, 1);
+
     public bool Touched { get { return touched; } }
     public bool Flicked { get { return flicked; } }
     public Vector2 FlickDirection { get { return positionTouchEnded - positionTouchBegan; } }
 
-    public float flickThresholdTime = .2f;
-
     [SerializeField] private bool touched = false;
     [SerializeField] private bool flicked = false;
+    private bool touching = false;
     private float timeTouchBegan;
     private Vector2 positionTouchBegan;
     private Vector2 positionTouchEnded;
@@ -46,17 +43,30 @@ public class CustomFloatingJoystick : FloatingJoystick
 
     public override void OnPointerDown(PointerEventData eventData)
     {
-        //
-        positionTouchBegan = eventData.position;
+        // タッチ可能領域外なら無視する
+        if (!GetTouchableScreenRect().Contains(eventData.position)) return;
 
+        touching = true;
+        positionTouchBegan = eventData.position;
         timeTouchBegan = Time.timeSinceLevelLoad;
 
+        //
         base.OnPointerDown(eventData);
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (!touching) return;
+
+        //
+        base.OnDrag(eventData);
     }
 
     public override void OnPointerUp(PointerEventData eventData)
     {
-        //
+        if (!touching) return;
+
+        touching = false;
         positionTouchEnded = eventData.position;
 
         if (Direction == Vector2.zero)
@@ -68,6 +78,25 @@ public class CustomFloatingJoystick : FloatingJoystick
             flicked = true;
         }
 
+        //
         base.OnPointerUp(eventData);
     }
+
+    private Rect GetTouchableScreenRect()
+    {
+        return new Rect(
+            touchableRect.x * Camera.main.pixelWidth,
+            touchableRect.y * Camera.main.pixelHeight,
+            touchableRect.width * Camera.main.pixelWidth,
+            touchableRect.height * Camera.main.pixelHeight);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Rect rect = GetTouchableScreenRect();
+
+        Gizmos.color = new Color(1, 0, 1, .3f);
+        Gizmos.DrawCube(rect.center, rect.size);
+    }
+
 }
