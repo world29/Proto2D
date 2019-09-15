c070780d4755a0e0939726799c961b6b22c68583
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-16 04:46:08 +0900

    ショット攻撃ノードを追加 (EnemyBehaviour スクリプトと連動)

---
+++ Assets/AI/BehaviourTree/Nodes/Actions/Shot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    public class Shot : Action
+    {
+        public Projectile projectilePrefab;
+
+        public override NodeStatus Evaluate(EnemyBehaviour context)
+        {
+            context.Shot(projectilePrefab);
+
+            m_nodeStatus = NodeStatus.SUCCESS;
+
+            return m_nodeStatus;
+        }
+
+        public override float GetProgress()
+        {
+            return 1;
+        }
+    }
+}
