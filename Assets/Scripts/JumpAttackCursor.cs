18ffaa3affc9098c98eaa609dec2810ed7796b99
world29 <ryo-29.2929@hotmail.co.jp>
2019-09-18 01:34:30 +0900

    マウスカーソルの方向に JumpAttackCursor を表示するようにした

---
+++ Assets/Scripts/JumpAttackCursor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAttackCursor : MonoBehaviour
+{
+    private Canvas canvas;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 targetDir = targetPosition - player.transform.position;
+        targetDir.x *= player.transform.localScale.x;
+
+        float angleDeg = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+
+        canvas.transform.localRotation = Quaternion.Euler(0, 0, angleDeg);
+    }
+}
