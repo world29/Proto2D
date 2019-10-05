83a9903979713a13f90229b16a20542443557c01
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-05 21:51:52 +0900

    ランダム部屋生成スクリプト追加

---
+++ Assets/Scripts/Map/MapController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public List<RoomController> startRoomPrefabs;
+    public List<RoomController> normalRoomPrefabs;
+
+    public PlayerController playerPrefab;
+
+    public Vector2 spawnAreaSize;
+    public float spawnAreaOffset = 10;
+
+    private Vector2 m_edgePosition = Vector2.zero;
+    private SpawnArea m_spawnArea;
+
+    private void Awake()
+    {
+        m_spawnArea = new SpawnArea(spawnAreaSize);
+        m_spawnArea.Update(Vector3.zero, spawnAreaOffset);
+    }
+
+    void Start()
+    {
+        // スタート部屋を生成
+        spawnNextRoom(getRandomStartRoom());
+
+        // プレイヤーを生成
+        //TODO: プレイヤーは GameController で生成する
+        GameObject playerSpawner = GameObject.FindGameObjectWithTag("PlayerSpawner");
+        GameObject.Instantiate(playerPrefab, playerSpawner.transform.position, Quaternion.identity);
+    }
+
+    void LateUpdate()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            m_spawnArea.Update(player.transform.position, spawnAreaOffset);
+        }
+
+        // プレイヤーの上方向に部屋を生成
+        while (m_spawnArea.IsIntersects(m_edgePosition))
+        {
+            spawnNextRoom(getRandomNormalRoom());
+        }
+    }
+
+    void spawnNextRoom(RoomController prefab)
+    {
+        RoomController room = GameObject.Instantiate(prefab, gameObject.transform);
+
+        float roomHeight = room.Size.y * room.CellSize.y;
+        room.gameObject.transform.Translate(0, m_edgePosition.y + room.OriginToCenter.y, 0);
+
+        m_edgePosition.y += roomHeight;
+    }
+
+    RoomController getRandomStartRoom()
+    {
+        if (startRoomPrefabs.Count > 0)
+        {
+            int index = Random.Range(0, startRoomPrefabs.Count);
+            return startRoomPrefabs[index];
+        }
+        return null;
+    }
+
+    RoomController getRandomNormalRoom()
+    {
+        if (normalRoomPrefabs.Count > 0)
+        {
+            int index = Random.Range(0, normalRoomPrefabs.Count);
+            Debug.Log(index);
+            return normalRoomPrefabs[index];
+        }
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // マップ上端
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(m_edgePosition, .5f);
+
+        // スポーン領域
+        Gizmos.color = new Color(0, 0, 1, .2f);
+        Gizmos.DrawCube(m_spawnArea.bounds.center, m_spawnArea.bounds.size);
+    }
+
+    struct SpawnArea
+    {
+        public Bounds bounds;
+
+        public SpawnArea(Vector2 size)
+        {
+            bounds = new Bounds(Vector3.zero, size);
+        }
+
+        public void Update(Vector3 targetPosition, float verticalOffset)
+        {
+            bounds.center = new Vector3(0, targetPosition.y + verticalOffset, 0);
+        }
+
+        public bool IsIntersects(Vector3 position)
+        {
+            Bounds targetBounds = new Bounds(position, Vector3.one);
+            return bounds.Intersects(targetBounds);
+        }
+    }
+}
---
+++ Assets/Scripts/Map/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Linq;
+
+public class RoomController : MonoBehaviour
+{
+    public Vector3Int Size { get { return primaryTilemap.size; } }
+    public Vector3 CellSize { get { return primaryTilemap.cellSize; } }
+    // タイルマップの左下端から (0,0) へのオフセット
+    public Vector3 OriginToCenter { get { return -primaryTilemap.CellToLocal(primaryTilemap.origin); } }
+
+    private Tilemap primaryTilemap;
+
+    private void Awake()
+    {
+        Tilemap tilemap = GetComponentInChildren<Tilemap>();
+        if (tilemap)
+        {
+            primaryTilemap = tilemap;
+            primaryTilemap.CompressBounds();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (primaryTilemap)
+        {
+            var minWorld = primaryTilemap.CellToWorld(primaryTilemap.cellBounds.min);
+            var maxWorld = primaryTilemap.CellToWorld(primaryTilemap.cellBounds.max);
+
+            Bounds bounds = new Bounds();
+            bounds.SetMinMax(minWorld, maxWorld);
+
+            Gizmos.color = new Color(1, 1, 0, .3f);
+            Gizmos.DrawCube(bounds.center, bounds.size);
+        }
+    }
+}
