71e040d431051f2dc6f6aa1dba3b697b2fc9a922
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-26 22:13:34 +0900

    SetProperty ノードを追加。ドロップダウンには全ての公開プロパティが表示されるが、動作するのは一部の型のみ。(int, float, bool, string)

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Actions/SetProperty.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Action/SetProperty")]
+    public class SetProperty : Action
+    {
+        [Tooltip("値を文字列で入力してください。\n次の型に対応: int, float, bool, string")]
+        public string m_valueString;
+
+        [HideInInspector, SerializeField]
+        public string m_propertyName;
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            System.Type type = typeof(EnemyBehaviour);
+            System.Reflection.FieldInfo fieldInfo = type.GetField(m_propertyName);
+            if (fieldInfo != null)
+            {
+                if (fieldInfo.FieldType == typeof(int))
+                {
+                    int value;
+                    if (int.TryParse(m_valueString, out value))
+                    {
+                        fieldInfo.SetValue(enemyBehaviour, value);
+                    }
+                }
+                else if (fieldInfo.FieldType == typeof(float))
+                {
+                    float value;
+                    if (float.TryParse(m_valueString, out value))
+                    {
+                        fieldInfo.SetValue(enemyBehaviour, value);
+                    }
+                }
+                else if (fieldInfo.FieldType == typeof(bool))
+                {
+                    bool value;
+                    if (bool.TryParse(m_valueString, out value))
+                    {
+                        fieldInfo.SetValue(enemyBehaviour, value);
+                    }
+                }
+                else if (fieldInfo.FieldType == typeof(string))
+                {
+                    fieldInfo.SetValue(enemyBehaviour, m_valueString);
+                }
+            }
+
+            return NodeStatus.SUCCESS;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Editor/NodeEditorSetProperty.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace Proto2D.AI
+{
+    [CustomNodeEditor(typeof(SetProperty))]
+    public class StateNodeEditorSetProperty : StateNodeEditor
+    {
+        private int m_selected = 0;
+        string[] m_options;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            SetProperty node = target as SetProperty;
+
+            // パブリックなプロパティをリストアップ
+            System.Type type = typeof(EnemyBehaviour);
+            var props = type.GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.DeclaredOnly);
+
+            m_options = props.Select(m => m.Name).ToArray();
+
+            int index = m_options.ToList().FindIndex(name => name == node.m_propertyName);
+            m_selected = index < 0 ? 0 : index;
+        }
+
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+
+            SetProperty node = target as SetProperty;
+
+            m_selected = EditorGUILayout.Popup("Property", m_selected, m_options);
+            node.m_propertyName = m_options[m_selected];
+        }
+    }
+}
