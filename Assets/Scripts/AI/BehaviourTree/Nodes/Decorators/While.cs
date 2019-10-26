7225fa95d4df9ab59dc81b69720e3ad45dbd3f9b
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-26 14:09:27 +0900

    While ノードを追加 (Condition に設定した関数が true を返す間、子ノードを実行し続ける。子ノードの返り値は無視される)

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Decorators/While.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Decorator/While")]
+    public class While : Decorator
+    {
+        [HideInInspector, SerializeField]
+        public string m_methodName;
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            System.Type type = enemyBehaviour.GetType();
+            System.Reflection.MethodInfo methodInfo = type.GetMethod(m_methodName);
+            Debug.Assert(methodInfo != null);
+
+            bool returnValue = (bool)methodInfo.Invoke(enemyBehaviour, new object[] { });
+            if (returnValue)
+            {
+                m_nodeStatus = NodeStatus.RUNNING;
+
+                m_node.Evaluate(enemyBehaviour);
+            }
+            else
+            {
+                m_nodeStatus = NodeStatus.FAILURE;
+            }
+
+            return m_nodeStatus;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Editor/NodeEditorWhile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace Proto2D.AI
+{
+    [CustomNodeEditor(typeof(While))]
+    public class StateNodeEditorWhile : StateNodeEditor
+    {
+        private int m_selected = 0;
+        string[] m_options;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            While whileNode = target as While;
+
+            // 引数がなく、戻り値が bool 型の public メンバ関数のみリストアップ
+            System.Type type = typeof(EnemyBehaviour);
+            var methods = type.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.DeclaredOnly);
+            var methods1 = methods.Where(m => m.ReturnType == typeof(bool) && m.GetParameters().Length == 0);
+
+            m_options = methods1.Select(m => m.Name).ToArray();
+
+            int index = m_options.ToList().FindIndex(name => name == whileNode.m_methodName);
+            m_selected = index < 0 ? 0 : index;
+        }
+
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+
+            While whileNode = target as While;
+
+            m_selected = EditorGUILayout.Popup("Condition", m_selected, m_options);
+            whileNode.m_methodName = m_options[m_selected];
+        }
+    }
+}
