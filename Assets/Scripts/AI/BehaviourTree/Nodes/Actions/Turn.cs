bf96ac7e817d88b498e50e33c4407b169536c3c7
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-26 22:25:00 +0900

    Turn ノードの仕様変更。向きのタイプを選べるようにした

--- Assets/Scripts/AI/BehaviourTree/Nodes/Actions/Turn.cs
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Actions/Turn.cs
@@ -7,9 +7,34 @@ namespace Proto2D.AI
     [CreateNodeMenu("BehaviourTree/Action/Turn")]
     public class Turn : Action
     {
+        public enum TurnType { TurnAround, LookAtPlayer, LookAtPlayerOpposite }
+
+        [Tooltip("TurnAround: 振り向く\nLookAtPlayer: プレイヤーのいる方を向く\nLookAtPlayerOpposite: プレイヤーのいる方と反対を向く")]
+        public TurnType m_turnType = TurnType.TurnAround;
+
         public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
         {
-            enemyBehaviour.Turn();
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+            switch (m_turnType)
+            {
+                case TurnType.TurnAround:
+                    enemyBehaviour.Turn();
+                    break;
+                case TurnType.LookAtPlayer:
+                    if (playerObject)
+                    {
+                        enemyBehaviour.LookAt(playerObject.transform);
+                    }
+                    break;
+                case TurnType.LookAtPlayerOpposite:
+                    if (playerObject)
+                    {
+                        enemyBehaviour.LookAt(playerObject.transform);
+                        enemyBehaviour.Turn();
+                    }
+                    break;
+            }
 
             return NodeStatus.SUCCESS;
         }
