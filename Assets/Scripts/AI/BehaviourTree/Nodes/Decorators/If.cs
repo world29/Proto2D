7815bfe11b6c1fa464cc8d9afb62e8dc569d20f4
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-31 03:06:39 +0900

    Decorator/If ノードを追加
    Conditional ノードで条件を指定する。割り込みに対応。

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Decorators/If.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Decorator/If")]
+    public class If : Decorator
+    {
+        [Tooltip("接続した Conditional ノードが SUCCESS なら実行します。"), Output(ShowBackingValue.Never, ConnectionType.Override)] public Node condition;
+
+        protected Node m_conditionNode;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            XNode.NodePort outPort = GetOutputPort("condition");
+            if (!outPort.IsConnected)
+            {
+                return;
+            }
+
+            m_conditionNode = outPort.Connection.node as Node;
+        }
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            if (m_nodeStatus == NodeStatus.RUNNING || m_conditionNode.Evaluate(enemyBehaviour) == NodeStatus.SUCCESS)
+            {
+                m_nodeStatus = m_node.Evaluate(enemyBehaviour);
+            }
+            else
+            {
+                m_nodeStatus = NodeStatus.FAILURE;
+            }
+
+            return m_nodeStatus;
+        }
+
+        //TODO: Decorator 基底クラスに移動する
+        public override void CollectConditionals(ref List<Conditional> conditionalNodes)
+        {
+            base.CollectConditionals(ref conditionalNodes);
+
+            conditionalNodes.Add(m_conditionNode as Conditional);
+        }
+    }
+}
