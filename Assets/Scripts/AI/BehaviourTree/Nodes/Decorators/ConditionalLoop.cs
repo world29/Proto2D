adf456d49b5f70c191e59028d4b306dfd4a29947
world29 <ryo-29.2929@hotmail.co.jp>
2019-10-30 23:48:06 +0900

    ConditionalLoop デコレータを追加 (While ノードをより柔軟にしたもの。条件を Conditional ノードで指定できる)

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Decorators/ConditionalLoop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Decorator/ConditionalLoop")]
+    public class ConditionalLoop : Decorator
+    {
+        [Tooltip("接続した Conditional ノードが SUCCESS である間、実行し続けます。"), Output(ShowBackingValue.Never, ConnectionType.Override)] public Node condition;
+
+        protected Node m_conditionNode;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            XNode.NodePort outPort = GetOutputPort("condition");
+            if (!outPort.IsConnected)
+            {
+                return;
+            }
+
+            m_conditionNode = outPort.Connection.node as Node;
+        }
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            if (m_conditionNode.Evaluate(enemyBehaviour) == NodeStatus.SUCCESS)
+            {
+                m_node.Evaluate(enemyBehaviour);
+
+                // 子ノードの実行結果によらず、RUNNING ステータスとする
+                m_nodeStatus = NodeStatus.RUNNING;
+            }
+            else
+            {
+                m_nodeStatus = NodeStatus.FAILURE;
+            }
+
+            return m_nodeStatus;
+        }
+    }
+}
