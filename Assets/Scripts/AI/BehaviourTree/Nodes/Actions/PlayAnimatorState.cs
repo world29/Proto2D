c7c1e306bee80df55aabdf6619b4dbf965807b3a
world29 <ryo-29.2929@hotmail.co.jp>
2019-11-18 03:43:01 +0900

    PlayAnimatorState ノードを追加。ステート名を指定して再生する。存在しないステート名を設定した場合のエラーチェックなし。パラメータによる遷移との相性悪い

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Actions/PlayAnimatorState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Action/PlayAnimatorState")]
+    public class PlayAnimatorState : Action
+    {
+        public string m_stateName;
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            Animator animator = enemyBehaviour.gameObject.GetComponent<Animator>();
+            if (animator == null)
+            {
+                Debug.LogWarning("Animator is not set");
+                return NodeStatus.FAILURE;
+            }
+
+            animator.Play(m_stateName);
+
+            return NodeStatus.SUCCESS;
+        }
+    }
+}
