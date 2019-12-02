76d026324e658fc691286fb9c9a3ef18554cb83f
world29 <ryo-29.2929@hotmail.co.jp>
2019-12-03 00:34:20 +0900

    Tier01, Tier02 のスタート部屋に TriggerSound オブジェクトを配置。プレイヤーがイベント領域に入ると BGM を再生する

---
+++ Assets/Scripts/Map/TriggerSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+    public class TriggerSound : MonoBehaviour
+    {
+        public AudioClip m_sound;
+
+        private GameController m_gameController;
+
+        private void Awake()
+        {
+            GameObject go = GameObject.FindGameObjectWithTag("GameController");
+            if (go)
+            {
+                m_gameController = go.GetComponent<GameController>();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                AudioSource audioSource = m_gameController.GetComponent<AudioSource>();
+                if (audioSource)
+                {
+                    audioSource.Stop();
+                    audioSource.clip = m_sound;
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
