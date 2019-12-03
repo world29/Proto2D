6e1e8972f031dd6fbbde36c9c40053c81b4a76ad
world29 <ryo-29.2929@hotmail.co.jp>
2019-12-04 02:23:10 +0900

    BGM の制御方法を変更
    - サウンド管理のモジュール SoundManager を追加
    - 指定領域にプレイヤーが入ると BGM の再生/停止を行う TriggerSoundPlay, TriggerSoundStop を追加

---
+++ Assets/Scripts/Map/TriggerSoundPlay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+    public class TriggerSoundPlay : MonoBehaviour
+    {
+        public AudioClip m_audioClip;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (SoundManager.Instance)
+                {
+                    SoundManager.Instance.Play(m_audioClip);
+                }
+
+                // BGM の変更は一度だけ行う
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            if (collider && collider.enabled)
+            {
+                Gizmos.color = new Color(1, 1, 0, .2f);
+                Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/TriggerSoundStop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+    public class TriggerSoundStop : MonoBehaviour
+    {
+        public float m_fadeOutDuration = 5;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (SoundManager.Instance)
+                {
+                    SoundManager.Instance.Stop(m_fadeOutDuration);
+                }
+
+                // BGM の変更は一度だけ行う
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            if (collider && collider.enabled)
+            {
+                Gizmos.color = new Color(1, 1, 0, .2f);
+                Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
+            }
+        }
+    }
+}
