75d2215a665c6ff803adb4d972cfbe6c6a1a2227
world29 <ryo-29.2929@hotmail.co.jp>
2019-12-10 00:14:00 +0900

    ミノタウロスの AI を更新 (視界を設定、アイドリング時はパトロールで、プレイヤーを見つけたら突進する)

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Conditionals/ConditionalCanMoveForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Conditional/CanMoveForward")]
+    public class ConditionalCanMoveForward : Conditional
+    {
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            m_nodeStatus = enemyBehaviour.CanMoveForward()
+                            ? NodeStatus.SUCCESS
+                            : NodeStatus.FAILURE;
+
+            return m_nodeStatus;
+        }
+    }
+}
