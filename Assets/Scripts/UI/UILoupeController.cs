2e6b18b1f0c20a2be50cdd62848d6b9f2df8eb71
world29 <ryo-29.2929@hotmail.co.jp>
2019-12-13 00:35:30 +0900

    ルーペを更新 (画面から見切れたときだけ表示するように対応)

---
+++ Assets/Scripts/UI/UILoupeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Proto2D
+{
+    public class UILoupeController : MonoBehaviour
+    {
+        public Image m_loupeImage;
+
+        private RectTransform m_rectTransform;
+        private PlayerController m_player;
+
+        private void Awake()
+        {
+            m_rectTransform = GetComponent<RectTransform>();
+        }
+        void Start()
+        {
+        }
+
+        void LateUpdate()
+        {
+            if (m_player == null)
+            {
+                GameObject go = GameObject.FindGameObjectWithTag("Player");
+                if (go)
+                {
+                    m_player = go.GetComponent<PlayerController>();
+                }
+            }
+
+            // プレイヤーがビューポートの外にいる場合のみルーペを表示する
+            bool isPlayerOutOfViewport = false;
+
+            if (m_player)
+            {
+                Vector3 viewportPoint = Camera.main.WorldToViewportPoint(m_player.transform.position);
+                if (viewportPoint.y < -0.05f)
+                {
+                    isPlayerOutOfViewport = true;
+
+                    // キャンバス座標 (= スクリーン座標)
+                    Vector3 pos = m_rectTransform.position;
+                    pos.x = Screen.width * viewportPoint.x;
+                    m_rectTransform.position = pos;
+                }
+            }
+
+            if (isPlayerOutOfViewport)
+            {
+                m_loupeImage.color = Color.white;
+            }
+            else
+            {
+                m_loupeImage.color = new Color(1, 1, 1, 0);
+            }
+        }
+
+        void UpdateUI()
+        {
+        }
+    }
+}
+
