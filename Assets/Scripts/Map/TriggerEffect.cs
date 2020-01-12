d3a692b1003b6ecfc77e86e5dc751a6d4ad7bfda
world29 <ryo-29.2929@hotmail.co.jp>
2020-01-12 17:37:42 +0900

    落ち葉や雪のパーティクルがステージと一緒に切り替わるようにした

---
+++ Assets/Scripts/Map/TriggerEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+    public class TriggerEffect : MonoBehaviour
+    {
+        public GameObject m_effectPrefab;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Transform root = Camera.main.transform;
+
+                // メインカメラの子オブジェクトを全削除
+                for (int i = 0; i < root.childCount; i++)
+                {
+                    Transform child = root.GetChild(i);
+                    Destroy(child.gameObject);
+                }
+                root.DetachChildren();
+
+                // パーティクルプレハブをメインカメラの子に設定
+                Instantiate(m_effectPrefab, root);
+
+                // BGM の変更は一度だけ行う
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+    }
+}
