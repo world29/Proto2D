f4347175c9be7a29aadea7287c817dd20564c55c
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-13 02:38:32 +0900

    速度一定の誘導ミサイルのテストシーン追加

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    public class Missile : MonoBehaviour
+    {
+        public Transform m_targetTransform;
+
+        [Header("速度")]
+        public float m_speed;
+
+        [Header("この係数が大きいほどターゲットに追従しやすく、Rigidbody2D.AngularDrag が大きいほど追従しにくくなる")]
+        public float m_ratio;
+
+        [Header("ターゲット位置を更新する")]
+        public bool m_updateTarget = false;
+
+        private Vector3 m_targetPosition;
+
+        void Start()
+        {
+            m_targetPosition = m_targetTransform.position;
+        }
+
+        private void Update()
+        {
+            if (m_updateTarget)
+            {
+                m_targetPosition = m_targetTransform.position;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            var rb = GetComponent<Rigidbody2D>();
+
+            var diff = m_targetPosition - transform.position;
+            Debug.DrawRay(transform.position, diff);
+            var q = Quaternion.FromToRotation(transform.right, diff);
+            var rot = (q.eulerAngles.z < 180) ? q.eulerAngles.z : q.eulerAngles.z - 360;
+            Debug.Log(rot);
+            var torque = rot * m_ratio;
+            rb.AddTorque(torque);
+
+            // 向いている方に進む
+            rb.velocity = transform.right * m_speed;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Application.isPlaying)
+            {
+                Gizmos.color = new Color(1, 0, 0, .3f);
+                Gizmos.DrawSphere(m_targetPosition, 1);
+            }
+        }
+    }
+}
