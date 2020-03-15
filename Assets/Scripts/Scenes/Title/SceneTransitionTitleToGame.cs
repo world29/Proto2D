873c4de157b952d454f1fb7647e835af8e1d14c4
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-16 07:40:08 +0900

    タイトルシーンの演出を微調整

--- Assets/Scripts/Scenes/Title/SceneTransitionTitleToGame.cs
+++ Assets/Scripts/Scenes/Title/SceneTransitionTitleToGame.cs
@@ -31,9 +31,21 @@ namespace Proto2D
                 {
                     float duration = 2;
 
-                    var t = m_titleLogo.DOFade(1, duration);
+                    var t = m_titleLogo.DOFade(1, duration).SetDelay(.5f);
 
                     sequence.Append(t);
+                }
+
+                {
+                    var rt = m_titleLogo.gameObject.GetComponent<RectTransform>();
+                    var targetPos = rt.position;
+
+                    float offsetY = -.5f;
+                    rt.position = new Vector3(targetPos.x, targetPos.y + offsetY, targetPos.z);
+
+                    var t = rt.DOLocalMove(targetPos, 1);
+
+                    sequence.Join(t);
                 }
 
 
@@ -47,7 +59,8 @@ namespace Proto2D
 
                     var t = m_camera.transform
                         .DOMove(targetPos, duration)
-                        .SetEase(easeType);
+                        .SetEase(easeType)
+                        .SetDelay(.5f);
 
                     sequence.Append(t);
                 }
