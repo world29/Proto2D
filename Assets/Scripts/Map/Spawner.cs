118a5e3d38a8dfa3a3a13fddd4bc77a5909b6d15
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-17 02:06:55 +0900

    設定されたプレハブからランダムにひとつインスタンス化するスポーナースクリプトを追加

---
+++ Assets/Scripts/Map/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Proto2D
+{
+    public class Spawner : MonoBehaviour
+    {
+        [Header("登録されたプレハブのうち、いずれかひとつが生成されます。")]
+        public GameObject[] m_prefabs;
+
+        private void Start()
+        {
+            Spawn();
+        }
+
+        [ContextMenu("Spawn prefab")]
+        public void Spawn()
+        {
+            if (m_prefabs.Length > 0)
+            {
+                var index = Random.Range(0, m_prefabs.Length);
+                var prefabToSpawn = m_prefabs[index];
+
+                GameObject.Instantiate(prefabToSpawn, transform.position, transform.rotation);
+            }
+        }
+    }
+}
