73eb7f31fdce7c2452f15bd9058029e0a126d1ab
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-17 02:17:25 +0900

    スポーナーで生成時の向きを設定できるようにした

--- Assets/Scripts/Map/Spawner.cs
+++ Assets/Scripts/Map/Spawner.cs
@@ -7,8 +7,13 @@ namespace Proto2D
 {
     public class Spawner : MonoBehaviour
     {
+        public enum FacingDirectionType { Front, Back, Random }
+
         [Header("登録されたプレハブのうち、いずれかひとつが生成されます。")]
         public GameObject[] m_prefabs;
+
+        [Header("生成時の向き (正面/背面/ランダム)")]
+        public FacingDirectionType m_facingDirectionType = FacingDirectionType.Front;
 
         private void Start()
         {
@@ -23,7 +28,28 @@ namespace Proto2D
                 var index = Random.Range(0, m_prefabs.Length);
                 var prefabToSpawn = m_prefabs[index];
 
-                GameObject.Instantiate(prefabToSpawn, transform.position, transform.rotation);
+                var go = GameObject.Instantiate(prefabToSpawn, transform.position, transform.rotation);
+
+                // 向きを設定する
+                {
+                    Vector3 localScale = Vector3.one;
+                    var type = m_facingDirectionType;
+
+                    if (type == FacingDirectionType.Random)
+                        type = (Random.value > .5f) ? FacingDirectionType.Front : FacingDirectionType.Back;
+
+                    switch (type)
+                    {
+                        case FacingDirectionType.Front:
+                            localScale.x = Mathf.Sign(transform.lossyScale.x);
+                            break;
+                        case FacingDirectionType.Back:
+                            localScale.x = Mathf.Sign(transform.lossyScale.x) * -1;
+                            break;
+                    }
+
+                    go.transform.localScale = localScale;
+                }
             }
         }
     }
