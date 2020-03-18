161f9f9ae84182f43a44f937103fcbb604d3b0f7
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-19 03:01:20 +0900

    SnakeLizard, ZweiLizard 更新
    ZweiLizard は ShotTransform をふたつ持つように変更

---
+++ Assets/Scripts/Emitter/MultiEmitterProxy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    // http://answers.unity.com/answers/1479775/view.html
+    public class TypeConstraintAttribute : PropertyAttribute
+    {
+        private System.Type type;
+
+        public TypeConstraintAttribute(System.Type type)
+        {
+            this.type = type;
+        }
+
+        public System.Type Type
+        {
+            get { return type; }
+        }
+    }
+
+    public class MultiEmitterProxy : MonoBehaviour
+    {
+        [TypeConstraint(typeof(IProjectileEmitter))]
+        public GameObject[] m_emitters;
+
+        // call from AnimationEvent
+        public void EmitByIndex(int emitterIndex)
+        {
+            Debug.Assert(emitterIndex < m_emitters.Length);
+
+            var emitter = m_emitters[emitterIndex].GetComponent<IProjectileEmitter>() as IProjectileEmitter;
+            emitter.Emit();
+        }
+    }
+}
