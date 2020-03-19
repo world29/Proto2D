2191760769dbc816506f1739a475bb4fb10d5d5e
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-20 05:27:41 +0900

    Revert "EnemyBehaviour.Shot() を削除"
    This reverts commit 219324889863395622e953cfbc322dd0a7d5d01d.

---
+++ Assets/Scripts/AI/BehaviourTree/Nodes/Actions/Shot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D.AI
+{
+    [CreateNodeMenu("BehaviourTree/Action/Shot")]
+    public class Shot : Action
+    {
+        public string m_stateName;
+        public Projectile projectilePrefab;
+
+        public override NodeStatus Evaluate(EnemyBehaviour enemyBehaviour)
+        {
+            Animator animator = enemyBehaviour.gameObject.GetComponent<Animator>();
+            if (m_stateName != "" && animator && animator.isActiveAndEnabled)
+            {
+                animator.Play(m_stateName);
+            }
+            enemyBehaviour.Shot(projectilePrefab);
+
+            return NodeStatus.SUCCESS;
+        }
+    }
+}
