f832722ad3404dccf2fff3b0e8209e4d04af32a5
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-20 16:06:17 +0900

    ディレクトリ階層の整理

---
+++ Assets/Sandbox/EventTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Proto2D
+{
+    public class EventTest : MonoBehaviour
+    {
+        public UnityEvent m_enterEvent;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                m_enterEvent.Invoke();
+            }
+        }
+
+        public void Print()
+        {
+            Debug.Log("test function");
+        }
+    }
+}
