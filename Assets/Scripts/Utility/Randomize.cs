54e9e32997313bb0316c0385777783bf5f1d8a04
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-20 15:58:57 +0900

    アイテムのリファクタリングに関連するスクリプトを追加

---
+++ Assets/Scripts/Utility/Randomize.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    static public class Randomize
+    {
+        // 異なる確率からひとつを選ぶ
+        // https://docs.unity3d.com/2018.4/Documentation/Manual/RandomNumbers.html
+        public static int ChooseWithProbabilities(float[] probs)
+        {
+            float total = 0;
+
+            foreach (float elem in probs)
+            {
+                total += elem;
+            }
+
+            float randomPoint = Random.value * total;
+
+            for (int i = 0; i < probs.Length; i++)
+            {
+                if (randomPoint < probs[i])
+                {
+                    return i;
+                }
+                else
+                {
+                    randomPoint -= probs[i];
+                }
+            }
+            return probs.Length - 1;
+        }
+    }
+}
