8233c9c77af2a2fd8cc564dec89553aaf38d5b36
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-21 06:15:43 +0900

    シーンを整理 (動作確認用シーンを Samples に移動, EnemySpawnerDemo シーンを追加)

---
+++ Assets/Scenes/Samples/TriggerArea.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class TriggerArea : MonoBehaviour
+    {
+        [SerializeField]
+        UnityEvent m_OnEnter;
+
+        private new BoxCollider2D collider { get { return GetComponent<BoxCollider2D>(); } }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                m_OnEnter.Invoke();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                m_OnEnter.Invoke();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0, 1, 1, .3f);
+            Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
+        }
+    }
+}
