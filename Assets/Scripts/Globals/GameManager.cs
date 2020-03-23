b0eca4b28e845f51820e70e296967bfc80f5d22b
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-24 04:57:31 +0900

    タイトル -> ゲーム -> ゲームオーバーのフローを管理するコンポーネントを追加

---
+++ Assets/Scripts/Globals/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Proto2D
+{
+    public class GameManager : MonoBehaviour
+    {
+        [System.NonSerialized]
+        public int m_currentStageNum = 0;
+
+        [SerializeField]
+        string[] m_stageName;
+        [SerializeField]
+        GameObject m_fadeCanvasPrefab;
+        [SerializeField]
+        GameObject m_gameOverCanvasPrefab;
+        [SerializeField]
+        float m_fadeWaitTime = 1;
+
+        GameObject m_fadeCanvasClone;
+        GameObject m_gameOverCanvasClone;
+        UIFadeCanvas m_fadeCanvas;
+        Button[] m_buttons;
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("Return key pressed. jump to next stage.");
+
+                NextStage();
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Escape key pressed. game over.");
+
+                GameOver();
+            }
+        }
+
+        // シーンのロード時に実行される
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+        }
+
+        public void NextStage()
+        {
+            m_currentStageNum++;
+            m_currentStageNum %= m_stageName.Length;
+
+            StartCoroutine(WaitForLoadScene(m_currentStageNum));
+        }
+
+        public void MoveToStage(int stageNum)
+        {
+            StartCoroutine(WaitForLoadScene(stageNum));
+        }
+
+        IEnumerator WaitForLoadScene(int stageNum)
+        {
+            // フェードオブジェクトを生成
+            m_fadeCanvasClone = Instantiate(m_fadeCanvasPrefab);
+
+            // コンポーネントを取得
+            m_fadeCanvas = m_fadeCanvasClone.GetComponent<UIFadeCanvas>();
+
+            // フェードイン
+            m_fadeCanvas.fadeIn = true;
+
+            yield return new WaitForSeconds(m_fadeWaitTime);
+
+            // シーンを非同期で読み込み
+            yield return SceneManager.LoadSceneAsync(m_stageName[stageNum]);
+
+            m_fadeCanvas.fadeOut = true;
+        }
+
+        public void GameOver()
+        {
+            m_gameOverCanvasClone = Instantiate(m_gameOverCanvasPrefab);
+
+            m_buttons = m_gameOverCanvasClone.GetComponentsInChildren<Button>();
+
+            m_buttons[0].onClick.AddListener(Retry);
+            m_buttons[1].onClick.AddListener(Return);
+        }
+
+        // 現在のシーンをリトライ
+        public void Retry()
+        {
+            Destroy(m_gameOverCanvasClone);
+
+            MoveToStage(m_currentStageNum);
+        }
+
+        // 最初のシーンに戻る
+        public void Return()
+        {
+            Destroy(m_gameOverCanvasClone);
+
+            MoveToStage(0);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIFadeCanvas.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Proto2D
+{
+    public class UIFadeCanvas : MonoBehaviour
+    {
+        [System.NonSerialized]
+        public bool fadeIn = false;
+
+        [System.NonSerialized]
+        public bool fadeOut = false;
+
+        [SerializeField]
+        Image m_panelImage;
+
+        [SerializeField]
+        float m_fadeSpeed = .02f;
+
+        float red, green, blue, alpha;
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            red = m_panelImage.color.r;
+            green = m_panelImage.color.g;
+            blue = m_panelImage.color.b;
+            alpha = m_panelImage.color.a;
+        }
+
+        private void Update()
+        {
+            if (fadeIn)
+            {
+                FadeIn();
+            }
+            else if (fadeOut)
+            {
+                FadeOut();
+            }
+        }
+
+        void FadeIn()
+        {
+            alpha += m_fadeSpeed;
+
+            SetAlpha();
+
+            if (alpha >= 1)
+            {
+                fadeIn = false;
+            }
+        }
+
+        void FadeOut()
+        {
+            alpha -= m_fadeSpeed;
+
+            SetAlpha();
+
+            if (alpha <= 0)
+            {
+                fadeOut = false;
+
+                Destroy(gameObject);
+            }
+        }
+
+        void SetAlpha()
+        {
+            m_panelImage.color = new Color(red, green, blue, alpha);
+        }
+    }
+}
