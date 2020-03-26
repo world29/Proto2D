32bb4731860cbaa4eaca23a1bdfedbe0ec3e5d36
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-27 01:05:19 +0900

    フォルダ構成の変更

---
+++ Assets/Scripts/Entity/EffectPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Proto2D
+{
+    public class EffectPlayer : MonoBehaviour
+    {
+        [Header("生成位置。null ならこのコンポーネントがアタッチされているオブジェクトの位置。")]
+        public Transform m_locator;
+
+        private void Awake()
+        {
+            if (m_locator == null)
+            {
+                m_locator = transform;
+            }
+        }
+
+        public void PlayEffect(GameObject effect)
+        {
+            Debug.Assert(m_locator != null);
+
+            GameObject.Instantiate(effect, m_locator.position, Quaternion.identity);
+        }
+    }
+}
