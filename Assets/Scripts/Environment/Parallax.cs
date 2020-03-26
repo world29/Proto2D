bb6c45e402ae0681d366d70c7cceddca378d9942
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-27 01:49:44 +0900

    カメラに 0~100% の割合で追従する Parallax コンポーネントを追加

---
+++ Assets/Scripts/Environment/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    public class Parallax : MonoBehaviour
+    {
+        [SerializeField, Range(0, 1)]
+        float m_parallaxEffect;
+
+        private Camera m_camera;
+        private Vector3 m_startPos;
+
+        private void Awake()
+        {
+            if (m_camera == null)
+            {
+                m_camera = Camera.main;
+            }
+        }
+
+        private void Start()
+        {
+            m_startPos = transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            float dist = m_camera.transform.position.y * m_parallaxEffect;
+
+            transform.position = new Vector3(transform.position.x, m_startPos.y + dist, transform.position.z);
+        }
+    }
+}
