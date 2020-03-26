17239d6986c0415bf1da951dc7609fe4ed3794d1
world29 <ryo-29.2929@hotmail.co.jp>
2020-03-27 01:05:32 +0900

    ボールをプレハブ化

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Proto2D
 {
     public class Entity : MonoBehaviour, IDamageSender, IDamageReceiver
     {
+        [SerializeField]
+        UnityEvent m_OnApplyDamage;
+
+        [SerializeField]
+        UnityEvent m_OnReceiveDamage;
+
         public void OnApplyDamage(DamageType type, float damage, GameObject receiver)
         {
-            //Debug.LogFormat("{0}: damage applied to {1}", gameObject.name, receiver.name);
+            Debug.LogFormat("{0}: damage applied to {1}", gameObject.name, receiver.name);
+
+            m_OnApplyDamage.Invoke();
         }
 
         public void OnReceiveDamage(DamageType type, float damage, GameObject sender)
         {
             Debug.LogFormat("{0}: damage received from {1}", gameObject.name, sender.name);
+
+            m_OnReceiveDamage.Invoke();
+        }
+
+        public void KillImmediately()
+        {
+            Destroy(gameObject);
         }
     }
 }
