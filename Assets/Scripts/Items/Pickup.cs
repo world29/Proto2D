55589558e752ec7d7a3b2f14d62f377c371bb51e
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-01 01:27:01 +0900

    Pickup コンポーネントのリファクタリングと各種アイテムへの反映

--- Assets/Scripts/Items/Pickup.cs
+++ Assets/Scripts/Items/Pickup.cs
@@ -2,18 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using DG.Tweening;
 
 namespace Proto2D
 {
     // 拾うことが出来るアイテムの共通基底クラス
     [RequireComponent(typeof(Rigidbody2D), typeof(Collision2D))]
-    public class Pickup : Entity
+    public class Pickup : MonoBehaviour
     {
         // 非推奨な UnityEngine.Component.rigidbody を無効化するため new で宣言
         [HideInInspector]
         public new Rigidbody2D rigidbody { get { return GetComponent<Rigidbody2D>(); } }
 
-        public UnityEvent m_OnPickup;
+        [SerializeField, Header("アイテムタイプ")]
+        ItemType m_itemType;
+
+        [SerializeField, Header("ピックアップイベント")]
+        UnityEvent m_OnPickup;
+
+        [SerializeField, Header("起動イベント")]
+        UnityEvent m_OnActivate;
+
+        [SerializeField, Header("取得後、再起動するまでの秒数 (0 なら再起動しない)")]
+        float m_reactivateInterval = 0;
+
+        // 取得先のタグはデフォルトでプレイヤーとする
+        string m_targetTag = "Player";
+
+        private void OnEnable()
+        {
+            m_OnActivate.Invoke();
+        }
 
         private void OnTriggerEnter2D(Collider2D collider)
         {
@@ -25,13 +45,61 @@ namespace Proto2D
             OnCollisionEvent(collision.collider);
         }
 
+        // コリジョンイベントの共通処理
         private void OnCollisionEvent(Collider2D collider)
         {
-            if (collider.gameObject.CompareTag("Player"))
+            if (collider.gameObject.CompareTag(m_targetTag))
             {
+                // ピックアップイベントの呼び出し
                 m_OnPickup.Invoke();
 
-                Destroy(gameObject);
+                // 取得先オブジェクトにイベントを送信
+                ExecuteEvents.Execute<IItemReceiver>(collider.gameObject, null,
+                    (target, eventTarget) => target.OnPickupItem(m_itemType, gameObject));
+
+                // 一定時間後にリスポーンする
+                if (m_reactivateInterval > 0)
+                {
+                    DOVirtual.DelayedCall(m_reactivateInterval, () => {
+                        gameObject.SetActive(true);
+                    });
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    // 再起動せず、自身を削除
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        // サウンドの再生
+        public void PlaySound(AudioClip clip)
+        {
+            if (Globals.SoundManager.Instance)
+            {
+                Globals.SoundManager.Instance.Play(clip);
+            }
+            else
+            {
+                Debug.LogWarning("Globals.SoundPlayer is not exists");
+            }
+        }
+
+        // エフェクトの生成
+        public void PlayEffect(GameObject effect)
+        {
+            GameObject.Instantiate(effect, transform.position, Quaternion.identity);
+        }
+
+        // デバッグ描画
+        private void OnDrawGizmos()
+        {
+            Collider2D collider = GetComponent<Collider2D>();
+            if (collider.enabled)
+            {
+                Gizmos.color = new Color(1, 1, 0, .3f);
+                Gizmos.DrawCube(collider.bounds.center, collider.bounds.size);
             }
         }
     }
