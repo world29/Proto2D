0904f89449b750b2567771c0db278a240d6679c2
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-02 02:08:16 +0900

    Rigidbody の速度に応じて、指定したオブジェクトを回転する機能を Rotator に切り分けた。Effect_Flame を修正

---
+++ Assets/Scripts/Projectiles/Rotator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField, Header("速度に応じて回転するオブジェクト")]
+        GameObject m_targetObject;
+
+        [SerializeField]
+        float m_offsetRotate = 0f;
+
+        Rigidbody2D m_rigidbody;
+
+        private void Start()
+        {
+            m_rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void LateUpdate()
+        {
+            if (m_targetObject)
+            {
+                //transform.rotation = Quaternion.identity;
+
+                float angleDeg = Mathf.Atan2(m_rigidbody.velocity.y, m_rigidbody.velocity.x) * Mathf.Rad2Deg;
+                float offset = m_offsetRotate;
+                if (transform.localScale.x < 0)
+                {
+                    offset -= 180;
+                }
+                m_targetObject.transform.Rotate(0, 0, transform.localScale.x * (angleDeg + offset - m_targetObject.transform.rotation.eulerAngles.z));
+            }
+        }
+    }
+}
