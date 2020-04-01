0dd71b71344ff3e4ba891e38844f93ea2d30a6d6
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-02 01:34:59 +0900

    デバッグ用スポーナーを追加

---
+++ Assets/Scenes/Samples/DebugSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    public class DebugSpawner : MonoBehaviour
+    {
+        public Projectile m_prefab;
+        public float m_initialSpeed;
+
+        public KeyCode m_spawnKey;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(m_spawnKey))
+            {
+                var projectile = GameObject.Instantiate(m_prefab, transform.position, transform.rotation) as Projectile;
+                projectile.rigidbody.velocity = transform.rotation * (Vector3.right * m_initialSpeed);
+            }
+        }
+    }
+}
