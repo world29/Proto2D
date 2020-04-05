4578479bfa898ccb69fd4f11bd861e6c80d23175
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-05 21:40:18 +0900

    TriggerArea: 起動回数の上限と、インターバルを追加

--- Assets/Scripts/Events/TriggerArea.cs
+++ Assets/Scripts/Events/TriggerArea.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UniRx;
+using System;
 
 namespace Proto2D
 {
-    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Collider2D))]
     public class TriggerArea : MonoBehaviour
     {
         [SerializeField]
         UnityEvent m_OnEnter;
 
-        private new BoxCollider2D collider { get { return GetComponent<BoxCollider2D>(); } }
+        [SerializeField, Header("起動回数の上限")]
+        int m_activationLimit = 1;
+
+        [SerializeField, Header("起動ごとのインターバル (秒)")]
+        Vector2 m_interval = Vector2.one;
+
+        private new Collider2D collider { get { return GetComponent<Collider2D>(); } }
+
+        int m_activatedCount;
+
+        private void Start()
+        {
+            m_activatedCount = 0;
+        }
 
         void Update()
         {
@@ -21,11 +36,48 @@ namespace Proto2D
             }
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerEnter2D(Collider2D collider)
         {
-            if (collision.gameObject.CompareTag("Player"))
+            OnCollisionEvent(collider);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            OnCollisionEvent(collision.collider);
+        }
+
+        private void OnTriggerStay2D(Collider2D collider)
+        {
+            OnCollisionEvent(collider);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            OnCollisionEvent(collision.collider);
+        }
+
+        protected virtual void OnCollisionEvent(Collider2D collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
             {
                 m_OnEnter.Invoke();
+
+                m_activatedCount++;
+
+                // 起動回数の上限に達したら削除
+                if (m_activatedCount >= m_activationLimit)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    // インターバルの間は無効化する
+                    var interval = UnityEngine.Random.Range(m_interval.x, m_interval.y);
+                    Observable.Timer(TimeSpan.FromSeconds(interval))
+                        .Subscribe(_ => gameObject.SetActive(true));
+
+                    gameObject.SetActive(false);
+                }
             }
         }
 
