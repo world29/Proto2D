a9c497ef017a8610bcec42c5ebb4000b285c1228
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-14 02:29:44 +0900

    HingeJoint2D を利用したロープのテストシーンを追加

---
+++ Assets/Scenes/Samples/RopeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proto2D
+{
+    public class RopeController : MonoBehaviour
+    {
+        Vector3 prev;
+
+        void Update()
+        {
+            Vector3 p = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+            p.z = 0;
+
+            if (Input.GetMouseButton(0))
+            {
+                GetComponent<Rigidbody2D>().velocity = (p - this.prev) * 80;
+            }
+            this.prev = p;
+        }
+    }
+}
