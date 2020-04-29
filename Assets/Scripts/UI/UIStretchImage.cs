bb6b1b7ceb03e0703484b1e86106d2557b00d41d
world29 <ryo-29.2929@hotmail.co.jp>
2020-04-29 17:11:53 +0900

    ステータス UI に勲章を追加 (クリア状態を反映する処理は後ほど実装)

---
+++ Assets/Scripts/UI/UIStretchImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Proto2D
+{
+    [ExecuteInEditMode]
+    public class UIStretchImage : MonoBehaviour
+    {
+        [SerializeField]
+        RectTransform m_sourceRect;
+
+        [SerializeField]
+        Image m_referenceImage;
+
+        [SerializeField]
+        RectTransform m_targetRect;
+
+        private void Update()
+        {
+            if (m_sourceRect && m_referenceImage && m_targetRect)
+            {
+                var srcSize = m_sourceRect.sizeDelta;
+                var refBounds = m_referenceImage.sprite.bounds;
+
+                var scale = Mathf.Min(srcSize.x / refBounds.size.x, srcSize.y / refBounds.size.y);
+
+                m_targetRect.sizeDelta = refBounds.size * scale;
+            }
+        }
+    }
+}
