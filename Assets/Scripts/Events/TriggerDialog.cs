f5d9ad4e043bac6a6c1f1a94fa85021453a5cc56
world29 <ryo-29.2929@hotmail.co.jp>
2020-05-04 16:45:28 +0900

    TitleScene にてインタラクションシステムの実装途中

---
+++ Assets/Scripts/Events/TriggerDialog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UniRx;
+using System;
+
+namespace Proto2D
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class TriggerDialog : MonoBehaviour
+    {
+        [SerializeField]
+        Canvas m_dialogPrefab;
+
+        private Canvas m_dialogClone;
+
+        const string TARGET_TAG = "Player";
+
+        private void Start()
+        {
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.gameObject.CompareTag(TARGET_TAG))
+            {
+                m_dialogClone = GameObject.Instantiate(m_dialogPrefab);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider.gameObject.CompareTag(TARGET_TAG))
+            {
+                if (m_dialogClone != null)
+                {
+                    Destroy(m_dialogClone.gameObject);
+                }
+            }
+        }
+    }
+}
