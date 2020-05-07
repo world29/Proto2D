eed7c0aa4eb23662f4300dac10f60cda36642401
world29 <ryo-29.2929@hotmail.co.jp>
2020-05-08 01:27:08 +0900

    HP ゲージの置き換え途中

---
+++ Assets/Scripts/UI/SlidableMask.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Proto2D
+{
+    public class SlidableMask : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 1f)]
+        float m_value;
+
+        RectTransform m_rectTransform;
+        Vector3 m_farBottom;
+        Vector3 m_farTop;
+
+        private void Awake()
+        {
+            m_rectTransform = GetComponent<RectTransform>();
+
+            m_farBottom = m_rectTransform.position - new Vector3(0f, m_rectTransform.rect.height);
+            m_farTop = m_rectTransform.position;
+        }
+
+        private void Start()
+        {
+            HandleSliderChanged(m_value);
+        }
+
+        private void LateUpdate()
+        {
+            HandleSliderChanged(m_value);
+        }
+
+        private void HandleSliderChanged(float value)
+        {
+            m_rectTransform.position = Vector2.Lerp(m_farBottom, m_farTop, value);
+        }
+    }
+}
