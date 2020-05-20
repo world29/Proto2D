fff80afb80b7af6f38c93c2ce82386120429f429
world29 <ryo-29.2929@hotmail.co.jp>
2020-05-21 05:50:25 +0900

    GameOver シーンで、タイムライン再生中のみスクリプトによるプレイヤー操作を無効化するようにした

---
+++ Assets/Timelines/PlayerUncontrollable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class PlayerUncontrollable : PlayableAsset
+{
+    [SerializeField]
+    RuntimeAnimatorController animatorController;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        PlayerUncontrollableBehaviour behaviour = new PlayerUncontrollableBehaviour();
+
+        behaviour.playerObject = go;
+        behaviour.animatorController = animatorController;
+
+        ScriptPlayable<PlayerUncontrollableBehaviour> playable = ScriptPlayable<PlayerUncontrollableBehaviour>.Create(graph, behaviour);
+
+        return playable;
+    }
+}
---
+++ Assets/Timelines/PlayerUncontrollableBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+// A behaviour that is attached to a playable
+public class PlayerUncontrollableBehaviour : PlayableBehaviour
+{
+    public GameObject playerObject;
+    public RuntimeAnimatorController animatorController;
+
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+        
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if (playerObject == null) { return; }
+
+        var controller = playerObject.GetComponent<PlayerController>();
+        controller.enabled = false;
+
+        var animator = playerObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = null;
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (playerObject == null) { return; }
+
+        var animator = playerObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = animatorController;
+
+        var controller = playerObject.GetComponent<PlayerController>();
+        controller.enabled = true;
+    }
+
+    // Called each frame while the state is set to Play
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+        
+    }
+}
