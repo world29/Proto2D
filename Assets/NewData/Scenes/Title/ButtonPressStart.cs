1c7f5e8b7df60aa695678cf3c920c62bba4a3d3c
world29 <623p6236p@gmail.com>
2022-06-24 22:53:26 +0900

    タイトル画面でボタンを押すとシーン遷移するようにした

---
+++ Assets/NewData/Scenes/Title/ButtonPressStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.NewData.Scripts.Presenters
+{
+    public class ButtonPressStart : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+
+        [SerializeField]
+        private string sceneNameTo = "Gameplay";
+
+        private void Start()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            SceneManager.LoadScene(sceneNameTo);
+        }
+    }
+}
