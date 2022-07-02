47d56af111dd9bf778c1b22d77e59c66366dd0d3
world29 <623p6236p@gmail.com>
2022-07-02 16:52:19 +0900

    タイトルロゴの演出を E キーでスキップできるようにした

---
+++ Assets/NewData/Scripts/Core/TimelineSkip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Assets.NewData.Scripts
+{
+    public class TimelineSkip : MonoBehaviour
+    {
+        private PlayableDirector currentDirector;
+        private bool isSceneSkipped = true;
+        private float timeToSkipTo;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !isSceneSkipped)
+            {
+                currentDirector.time = timeToSkipTo;
+                isSceneSkipped = true;
+            }
+        }
+
+        public void GetDirector(PlayableDirector director)
+        {
+            isSceneSkipped = false;
+            currentDirector = director;
+        }
+
+        public void GetSkipTime(float skipTime)
+        {
+            timeToSkipTo = skipTime;
+        }
+    }
+}
