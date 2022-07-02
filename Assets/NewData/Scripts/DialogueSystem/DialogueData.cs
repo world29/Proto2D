539f6efc5f7abaf60b80115264e06666aff8e7cb
world29 <623p6236p@gmail.com>
2022-07-02 23:24:30 +0900

    会話システムの基本実装を追加

---
+++ Assets/NewData/Scripts/DialogueSystem/DialogueData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets.NewData.Scripts
+{
+    [CreateAssetMenu(menuName = "Dialogue/DialogueData")]
+    public class DialogueData : ScriptableObject
+    {
+        [SerializeField]
+        [TextArea]
+        private string[] dialogueText;
+
+        public string[] Text => dialogueText;
+    }
+}
---
+++ Assets/NewData/Scripts/DialogueSystem/DialoguePlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+namespace Assets.NewData.Scripts
+{
+    public class DialoguePlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject dialogueBox;
+
+        [SerializeField]
+        private TMP_Text textLabel;
+
+        [SerializeField]
+        private DialogueData _dialogueData;
+
+        private TypewriterEffect typewriterEffect;
+
+        private void Start()
+        {
+            TryGetComponent<TypewriterEffect>(out typewriterEffect);
+            CloseDialogueBox();
+            ShowDialogue(_dialogueData);
+        }
+
+        public void ShowDialogue(DialogueData dialogueData)
+        {
+            dialogueBox.SetActive(true);
+            StartCoroutine(StepThroughDialogue(dialogueData));
+        }
+
+        private IEnumerator StepThroughDialogue(DialogueData dialogueData)
+        {
+            foreach (string text in dialogueData.Text)
+            {
+                yield return typewriterEffect.Run(text, textLabel);
+                yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            }
+
+            CloseDialogueBox();
+        }
+
+        private void CloseDialogueBox()
+        {
+            dialogueBox.SetActive(false);
+            textLabel.text = string.Empty;
+        }
+    }
+}
