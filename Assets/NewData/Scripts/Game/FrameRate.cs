0954bebf11d44e0a7c2d7325a892a3c8a8c4265e
world29 <623p6236p@gmail.com>
2022-08-07 15:25:14 +0900

    垂直同期を OFF にし、フレームレートを60に設定

---
+++ Assets/NewData/Scripts/Game/FrameRate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.NewData.Scripts
+{
+    // フレームレートを固定する
+    public class FrameRate : MonoBehaviour
+    {
+        [SerializeField]
+        private int frameRate = 60;
+
+        void Awake()
+        {
+            Application.targetFrameRate = frameRate;
+        }
+    }
+}
