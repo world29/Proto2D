eae13971da990a2cf9b4d1c7308344aaf0f92c69
world29 <623p6236p@gmail.com>
2022-08-27 22:26:08 +0900

    ジャンプ台（HopMushroom）を調整

---
+++ Assets/NewData/Scripts/Gimmicks/EffectorJump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.NewData.Scripts
+{
+    // 衝突するとプレイヤーに上方向の速度を与えるエフェクタ
+    public class EffectorJump : MonoBehaviour
+    {
+        [SerializeField]
+        private float jumpForce = 10f;
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            TryGetComponent(out _animator);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("EffectorJump.OnTriggerEnter");
+
+                if (other.TryGetComponent(out IPlayerMove playerMove))
+                {
+                    playerMove.Velocity = new Vector2(0, jumpForce);
+
+                    _animator.SetTrigger("action");
+                }
+            }
+        }
+    }
+}
